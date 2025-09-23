6cf210cf43c3a6871c614c1cdcdc6911a969ad6d
Andy <abobkof@mail.ru>
2025-09-23 23:21:57 +0600

    lab2 in progress

--- afins.py
+++ afins.py
@@ -7,16 +7,113 @@ class Afins:
         self.index_to_letter = {i: char for i,
                                 char in enumerate(self.alphabet)}
 
-    def nod(self, a: int, b: int) -> int:
+    def cleaner(self, text: str) -> str:
+        text = text.lower().replace("ё", "е")
+        return "".join(char for char in text if char.isalpha())
+
+    def word_funder(self, text: str):
+        mini_words = {
+            "а", "и", "но", "что", "кто", "как", "это", "не", "он", "она", "оно", "мы", "вы", "они", "судьи", "тот", "есть",
+            "да", "ли", "бы", "же", "вы", "ты", "у", "по", "на", "от", "за", "для", "о", "в", "к", "из", "так", "вот", "там", "раз"
+        }
+
+        count = 0
+        for word in mini_words:
+            if word in text:
+                count += 1
+        return count
+
+    def gcd(self, a: int, b: int) -> tuple:
         a, b = (a, b) if a > b else (b, a)
+
+        if b == 0:
+            return 0
 
         while b != 1:
             if a % b == 0:
-                return 0
+                return b
 
             a, b = b, a % b
 
         return b
 
-    def reciprocal(self, a: int, b: int) -> tuple[int, bool]:
-        ...
+    def inv(self, a: int, b: int) -> int:
+        if self.gcd(a, b) == 1:
+            return pow(a % b, -1, b)
+        return 0
+
+    def solve_comparison(self, a: int, b: int, m: int) -> tuple[int, int]:
+        a %= m
+        b %= m
+
+        if a == 0:
+            if b == 0:
+                return list(range(m))
+            return []
+
+        d = self.gcd(a, m)
+
+        if b % d != 0:
+            return []
+
+        a //= d
+        b //= d
+        m_new = m // d
+
+        inv = self.inv(a, m_new)
+        x = b * inv % m_new
+        solutions = [(x + k * m_new) % m for k in range(d)]
+        return sorted(solutions)
+
+    def solve_system(self, a: int, b: int, c: int, d: int, m: int) -> list[tuple[int, int]]:
+        a %= m
+        b %= m
+        c %= m
+        d %= m
+
+        a0 = (a - c) % m
+        b0 = (b - d) % m
+
+        if a0 == 0:
+            if b0 != 0:
+                return []
+
+            return [(x, (b - a * x) % m) for x in range(m)]
+
+        x_c = self.solve_comparison(a0, b0, m)
+        if not x_c:
+            return []
+
+        solutions = [(x, (b - a * x) % m) for x in x_c]
+        return sorted(solutions)
+
+    def analyze(self, text: str) -> dict[float: list[str]]:
+        frequencies = {}
+        for char in text:
+            if char in frequencies:
+                frequencies[char] += 1
+            else:
+                frequencies[char] = 1
+
+        for char in frequencies:
+            frequencies[char] /= len(text)
+
+        chars = {}
+        for char in frequencies:
+            freq = frequencies[char]
+            if freq not in chars:
+                chars[freq] = [char]
+            else:
+                chars[freq].append(char)
+
+        return chars
+    
+    def give_pairs(self, chars: dict) -> list[tuple[int, int]]:
+        all_chars = []
+        for freq in chars:
+            all_chars += chars[freq]
+        
+        pairs = []
+        for i in range(8):
+            for j in range(3):
+                ...
--- main.py
+++ main.py
@@ -1,6 +1,130 @@
+import os
 from afins import Afins
 
-if __name__ == "__main__":
+
+def main():
+    fraze = ''.join(["пгсаистшгшэюыабтюфюцьцчзсгшдэсдшнвэнчгстзрюь",
+                     "апзскмзстшщгчадэсныскщювэсепкнюенсввачнпаьвн",
+                     "стсгшпыпасвэлемчгяпымзпфрнчанюаюцлбтсщпбсышф",
+                     "чтшглмсвашяцчстпгрцювэзчячвлцюэзютшьнюцйчонч",
+                     "ашпеаюнюэшмзпюыапзчнцсгшяцспцюсвмсзпнчкиашмйч"])
     afins = Afins()
 
-    print(afins.nod(8, 32))
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        print("-------|| Афинный шифр ||-------\n")
+        print("   Главное меню")
+        print("1. Получить обратный элемент")
+        print("2. Решить сравнение")
+        print("3. Решить систему")
+        print("4. Выполнить анализ")
+        print("5. Зашифровать")
+        print("6. Расшифровать")
+        print("7. Ответ")
+        print("8. Выход")
+
+        action = input('Выберите действие (введите цифру): ')
+
+        if action == '1':
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print("-------|| Афинный шифр ||-------")
+            print("   Получить обратный элемент\n")
+
+            a = int(input("Введите элемент: "))
+            b = int(input("Введите модуль: "))
+
+            print("[I] Получение обратного элемента")
+            inv = afins.inv(a, b)
+            if inv:
+                print("[I] Обратный элемент получен\n")
+
+                print(f"Элмент обратный данному: {inv}")
+            else:
+                print("[E] Введённый элемент необратим в данном кольце")
+
+            input("Нажмите Enter для продолжения...")
+
+        elif action == '2':
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print("-------|| Афинный шифр ||-------")
+            print("        Решить сравнение\n")
+
+            print("Сравнение вида <ax mod m == b>")
+            a = int(input("Введите a: "))
+            b = int(input("Введите b: "))
+            m = int(input("Введите m: "))
+
+            print(f"[I] Решение сравнения <{a}x mod {m} == {b}>")
+            solutions = afins.solve_comparison(a, b, m)
+            if solutions:
+                print("[I] Сравнение решено")
+                print(f"[I] Найдено {len(solutions)} решений\n")
+
+                for i, solution in enumerate(solutions, 1):
+                    print(f"Решение {i}: {solution}")
+            else:
+                print("[E] Не найдено решений")
+
+            input("Нажмите Enter для продолжения...")
+
+        elif action == '3':
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print("-------|| Афинный шифр ||-------")
+            print("         Решить систему\n")
+
+            print("Система вида:")
+            print("(ax + y) mod m == b")
+            print("(cx + y) mod m == d")
+            a = int(input("Введите a: "))
+            b = int(input("Введите b: "))
+            c = int(input("Введите c: "))
+            d = int(input("Введите d: "))
+            m = int(input("Введите m: "))
+
+            print(
+                f"[I] Решение системы <{a}x mod {m} == {b}> <{c}x mod {m} == {d}>")
+            solutions = afins.solve_system(a, b, c, d, m)
+            if solutions:
+                print("[I] Система решена")
+                print(f"[I] Найдено {len(solutions)} решений\n")
+
+                for i, solution in enumerate(solutions, 1):
+                    print(f"Решение {i}: x = {solution[0]} y = {solution[1]}")
+            else:
+                print("[E] Не найдено решений")
+
+            input("Нажмите Enter для продолжения...")
+
+        elif action == '4':
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print("-------|| Афинный шифр ||-------")
+            print("        Выполнить анализ\n")
+
+            text = input(
+                "Введите фразу для анализа (Enter для использования заготовленной):\n")
+            text = fraze if not text else text
+
+            print("[I] Анализ фразы")
+            chars = afins.analyze(text)
+            print("[I] Анализ завершён\n")
+
+            print("Таблица частот:")
+            for freq in chars:
+                for char in chars[freq]:
+                    print(f"{char}: {freq}", end=' | ')
+                print()
+
+            input("Нажмите Enter для продолжения...")
+
+        elif action == '8' or action == "":
+            print("\n[I] Завершение работы программы")
+            break
+
+
+if __name__ == "__main__":
+    main()
