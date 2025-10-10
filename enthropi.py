222528346447fe9195847cf6d24df71034adddd9
vaiforic-nt-win <abobkof@mail.ru>
2025-10-10 11:28:45 +0600

    lab3 ready

---
+++ enthropi.py
@@ -0,0 +1,53 @@
+import math
+
+
+class Enthropi:
+    def __init__(self):
+        self.alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+    def cleaner(self, text: str) -> str:
+        return "".join(char for char in text.lower() if char.isalpha() and char in self.alphabet)
+
+    def give_k_gramms(self, symbols: str, k: int) -> list:
+        k_gramms = []
+        len_symbols = len(symbols)
+        for i in range(len_symbols - k + 1):
+            k_gramms.append(symbols[i:i + k])
+        return sorted(set(k_gramms))
+
+    def k_gramms_generator(self, text: str, k_end: int, k_start: int = 1) -> dict[int: str]:
+        text = self.cleaner(text)
+        all_k_gramms = {}
+        for k in range(k_start, k_end + 1):
+            all_k_gramms[k] = self.give_k_gramms(text, k)
+        return all_k_gramms
+
+    def finder(self, text: str, gramm: str, k: int) -> int:
+        count = 0
+        for i in range(len(text) - k + 1):
+            if gramm == text[i:i + k]:
+                count += 1
+        return count
+
+    def get_frequencies(self, text: str, k_gramms: list, k: int) -> list:
+        frequencies = {}
+        for k_gramm in k_gramms:
+            frequencies[k_gramm] = self.finder(text, k_gramm, k)
+        all_count = sum(list(frequencies.values()))
+        for k_gramm in k_gramms:
+            frequencies[k_gramm] = frequencies[k_gramm] / all_count
+        return list(frequencies.values())
+
+    def analyze(self,  text: str, all_k_gramms: dict) -> dict:
+        text = self.cleaner(text)
+        all_frequencies = {}
+        for k in all_k_gramms:
+            k_gramms = all_k_gramms[k]
+            all_frequencies[k] = self.get_frequencies(text, k_gramms, k)
+        return all_frequencies
+
+    def enthropi(self, frequencies: dict, k: int):
+        HkT = 0
+        for freq in frequencies:
+            HkT += freq * math.log2(freq)
+        return -HkT / k
---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+from enthropi import Enthropi
+import matplotlib.pyplot as plt
+
+
+def main():
+    fraze = ''.join([
+        "Несчастная кошка порезала лапу – Сидит, и ни шагу не может сту",
+        "пить. Скорей, чтобы вылечить кошкину лапу Воздушные шарики надо",
+        " купить! И сразу столпился народ на дороге – Шумит, и кричит, и на",
+        " кошку глядит. А кошка отчасти идет по дороге, Отчасти по воздуху",
+        " плавно летит!"
+    ])
+
+    enthropi = Enthropi()
+
+    hktks = []
+
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        print("-------|| Модель открытого текста ||-------\n")
+        print("   Главное меню")
+        print("1. Рассчитаь Hk(T)/k")
+        print("2. Построить график")
+        print("3. Выход")
+
+        action = input('Выберите действие (введите цифру): ')
+
+        if action == '1':
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print("-------|| Модель открытого текста ||-------")
+            print("             Рассчитаь Hk(T)/k\n")
+
+            print("[I] Поиск всех k-грамм от 1 до 5")
+            all_k_gramms = enthropi.k_gramms_generator(fraze, 5)
+            print("[I] Все k-граммы найдены")
+
+            print("[I] Расчёт частот")
+            all_frequencies = enthropi.analyze(fraze, all_k_gramms)
+            print("[I] Частоты найдены")
+
+            print("[I] Расчёт Hk(T)/k от 1 до 5")
+            hktks.clear()
+            for k in all_frequencies:
+                frequencies = all_frequencies[k]
+                hktks.append(enthropi.enthropi(frequencies, k))
+            print("[I] Расчёт завершён\n")
+
+            print("Hk(T)/k при:")
+            for k in range(len(hktks)):
+                print(f"k = {k + 1}: {hktks[k]}")
+
+            input("\nНажмите Enter для продолжения...")
+
+        elif action == '2':
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print("-------|| Модель открытого текста ||-------")
+            print("             Построить график\n")
+
+            plt.plot(range(1, 6), hktks, marker='o')
+            plt.title("Зависимость Hk(T)/k от k")
+            plt.xlabel("k")
+            plt.ylabel("Hk(T)/k")
+            plt.grid(True)
+            plt.show()
+
+            input("Нажмите Enter для продолжения...")
+
+        elif action == '8' or action == "":
+            print("\n[I] Завершение работы программы")
+            break
+
+
+if __name__ == "__main__":
+    main()
